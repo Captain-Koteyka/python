b2468d63492b9fb44328ebbdc343da70a340236c
Alice <kulakovaalisa01@gmail.com>
2020-04-14 19:04:38 +0300

    task 3 list completed

---
+++ task 3 list.py
@@ -0,0 +1,10 @@
+seasons = ('winter', 'winter',
+           'spring', 'spring', 'spring',
+           'summer', 'summer', 'summer',
+           'autumn', 'autumn', 'autumn',
+           'winter')
+month = input()
+if month.isdigit():
+    index = int(month)
+    if 1 <= index <= 12:
+        print(seasons[index - 1])
