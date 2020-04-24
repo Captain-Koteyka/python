71e2525a18ea6eac083b56fdc3efc0d23969ffe3
Alice <kulakovaalisa01@gmail.com>
2020-04-24 17:25:31 +0300

    les5_task2 completed

---
+++ les5_task2.py
@@ -0,0 +1,10 @@
+with open("new_file.txt", "r", encoding='UTF-8') as my_file:
+    number_of_lines = 0
+    number_of_words = 0
+    for line in my_file:
+        number_of_lines += 1
+        for word in line.split(' '):
+            number_of_words += 1
+        print('Количество слов в строке ', number_of_lines, ': ', number_of_words, sep='')
+        number_of_words = 0
+print('Количество строк:', number_of_lines)
