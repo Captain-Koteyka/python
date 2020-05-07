e3e9c2019c9dffe73b9b2f233f5f95b10ce3f790
Alice <kulakovaalisa01@gmail.com>
2020-05-07 20:54:58 +0300

    les8_task4-6 completed

--- les8_task4-6.py
+++ les8_task4-6.py
@@ -1,3 +1,8 @@
+class NotDigitError(Exception):
+    def __init__(self, text):
+        self.text = text
+
+
 class Warehouse:
     def __init__(self, name, price, quantity):
         self.name = name
@@ -5,16 +10,47 @@ class Warehouse:
         self.quantity = quantity
 
     @staticmethod
-    def reception(self):
-        reception_dict = {}
-        try:
-            name = input('Введите название')
-            price = input('Введите цену')
-        reception_dict[self.name] = self.price
-        return reception_dict
+    def reception():
+        warehouse_dict = {}
+        while True:
+            name = input('Введите наименование или stop для окончания ввода\n')
+            if name == 'stop':
+                break
+            try:
+                price = input('Введите цену или stop для окончания ввода\n')
+                if price.isdigit():
+                    price = int(price)
+                elif price == 'stop':
+                    break
+                else:
+                    raise NotDigitError('Ошибка: введена строка\n')
+            except NotDigitError as error:
+                print(error)
+                continue
+            quantity = input('Введите количество или stop для окончания ввода\n')
+            try:
+                if quantity.isdigit():
+                    quantity = int(quantity)
+                    warehouse_dict[name] = price, quantity
+                elif quantity == 'stop':
+                    break
+                else:
+                    raise NotDigitError('Ошибка: введена строка\n')
+            except NotDigitError as error:
+                print(error)
+        return warehouse_dict
 
-    def transfer(self):
-        pass
+    @staticmethod
+    def transfer(reception: dict):
+        transfer_dict = {}
+        while True:
+            k = input('Введите название предмета для перемещения со склада или stop для окончания ввода\n')
+            if k == 'stop':
+                break
+            else:
+                transfer_dict[k] = reception[k]
+                del reception[k]
+        return transfer_dict
 
 
 class OfficeEquipment:
@@ -42,5 +78,6 @@ class Copier(OfficeEquipment):
         super().__init__(name, price, quantity)
 
 
-printer = Printer('A4', 'fff', 654, 15)
-print(printer.reception)
+equipment = Warehouse.reception()
+print(Warehouse.transfer(equipment))
+print(equipment)
